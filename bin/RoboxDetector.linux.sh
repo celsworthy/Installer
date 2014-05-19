0863acf94ae17a59a68353f3c48888626df7e272
Tony Lynch <tony@cel-uk.com>
2014-05-19 17:18:07 +0100

    Fixes ROB-88

--- bin/RoboxDetector.linux.sh
+++ bin/RoboxDetector.linux.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-output=`lsusb -d$1`
+output=`ls  /dev/serial/by-id/ | grep $1 | grep $2`
 if [ "$output" = "" ]
 then
 	echo "NOT_CONNECTED"
 else
+        if [ -h /dev/serial/by-id/"$output" ]
+        then
+            output=`readlink -f /dev/serial/by-id/"$output"`
+        fi
 	echo $output
 fi
