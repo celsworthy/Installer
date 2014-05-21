d96adfc9ed2da90ae145b44f2e9ac3efadeed0ea
Tony Lynch <tony@cel-uk.com>
2014-05-21 11:14:50 +0100

    Compress osx app dir after installer is built

--- build_osx_automaker.sh
+++ build_osx_automaker.sh
@@ -1,3 +1,3 @@
 /opt/installbuilder-9.0.1/bin/builder build ./AutoMaker.xml osx
-zip -r LatestBuilds/AutoMaker-0.10.07-osx-installer.app AutoMaker-0.10.07-osx-installer.app
+zip -r LatestBuilds/AutoMaker-0.10.07-osx-installer.app LatestBuilds/AutoMaker-0.10.07-osx-installer.app
 rm -rf LatestBuilds/AutoMaker-0.10.07-osx-installer.app
