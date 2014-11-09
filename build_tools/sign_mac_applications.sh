c6170e4e5c4e2fd70de17ef9fbebf688f474a13a
Ian Hudson <ian@cel-uk.com>
2014-11-09 00:00:45 +0000

    Updated Mac signing script

--- build_tools/sign_mac_applications.sh
+++ build_tools/sign_mac_applications.sh
@@ -1,1 +1,5 @@
-osxsigner --identity "3rd Party Mac Developer Application" --identifier "celtech.AutoMaker" --output /tmp --resource-rules "resource-rules.plist" $1/AutoMaker-$2-osx-installer.app
+/Applications/BitRock\ InstallBuilder\ Professional\ 9.5.2/tools/code-signing/osx/osxsigner --identity "3rd Party Mac Developer Application" --identifier "celtech.AutoMaker" --output /tmp $1/AutoMaker-$2-osx-installer.app/
+
+zip -r /tmp/AutoMaker-$2-osx-installer.app.zip /tmp/AutoMaker-$2-osx-installer.app/
+
+scp -i ~/.ssh/BuildServer.pem /tmp/AutoMaker-$2-osx-installer.app.zip ubuntu@downloads.cel-robox.com:/home/ubuntu
