394a7db03c8f27d6d050ef5ee1e394e572dc0d80
ianphudson <ian@cel-uk.com>
2015-03-25 16:47:21 +0000

    Update to certificates

--- build_tools/sign_windows_applications.bat
+++ build_tools/sign_windows_applications.bat
@@ -1,3 +1,3 @@
-signtool sign /v /d "CEL AutoMaker" /f %~dp0CELCodeSigningCert.p12 /p slat3d /t http://timestamp.verisign.com/scripts/timestamp.dll AutoMaker-%1-windows-installer.exe
-signtool sign /v /d "CEL Configurator" /debug /f %~dp0CELCodeSigningCert.p12 /p slat3d /t http://timestamp.verisign.com/scripts/timestamp.dll Configurator-%1-windows-installer.exe
-signtool sign /v /d "CEL Commissionator" /f %~dp0CELCodeSigningCert.p12 /p slat3d /t http://timestamp.verisign.com/scripts/timestamp.dll %1-windows-installer.exe
+signtool sign /v /d "CEL AutoMaker" /f %~dp0CELCodeSigningCert-Exp-18_03_17.p12 /p slat3d /t http://timestamp.verisign.com/scripts/timestamp.dll AutoMaker-%1-windows-installer.exe
+signtool sign /v /d "CEL Configurator" /debug /f %~dp0CELCodeSigningCert-Exp-18_03_17.p12 /p slat3d /t http://timestamp.verisign.com/scripts/timestamp.dll Configurator-%1-windows-installer.exe
+signtool sign /v /d "CEL Commissionator" /f %~dp0CELCodeSigningCert-Exp-18_03_17.p12 /p slat3d /t http://timestamp.verisign.com/scripts/timestamp.dll %1-windows-installer.exe
