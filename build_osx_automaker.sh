5564738639f52ab1dc14d0722fba7185cebd6e64
Ian Hudson <ian@cel-uk.com>
2016-06-20 11:12:49 +0100

    Reorganised directories and included language files in installation

--- build_osx_automaker.sh
+++
@@ -1,8 +0,0 @@
-/opt/installbuilder-9.0.1/bin/builder build ./AutoMaker.xml osx
-set -x
-ls -al LatestBuilds
-
-cd LatestBuilds
-ls -al
-zip -v -r AutoMaker-osx-installer.app AutoMaker-0.10.07-osx-installer.app
-#rm -rf LatestBuilds/AutoMaker-0.10.07-osx-installer.app
