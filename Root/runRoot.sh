55f2c4069bc8c5d0e54a3293012704e1aca678ba
Ian Hudson <ian@cel-uk.com>
2016-07-27 14:05:54 +0100

    Corrected ref to Root.yml

--- Root/runRoot.sh
+++ Root/runRoot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java/bin/java -Djava.net.preferIPv4Stack=true -DlibertySystems.configFile=Root.configFile.xml -jar Root.jar server root.yml
+java/bin/java -Djava.net.preferIPv4Stack=true -DlibertySystems.configFile=Root.configFile.xml -jar Root.jar server Root.yml
