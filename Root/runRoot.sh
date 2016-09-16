27384937fd75709379bf7c854dd741ab5b8bd3b7
Ian Hudson <ian@cel-uk.com>
2016-09-16 09:32:04 +0100

    Added root auto-upgrade for pi/linux/mac

--- Root/runRoot.sh
+++ Root/runRoot.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
+
+rootUpgradeFileBase="/tmp/RootARM*"
+
+#Handle pre-run upgrade
+for f in $rootUpgradeFileBase; do
+    if [ -e "$f" ]
+    then
+	upgradeFile=$(ls -t $rootUpgradeFileBase | head -1)
+	echo unzip $upgradeFile -d ..
+	rm -f $rootUpgradeFileBase
+    fi
+    break
+done
+
 java/bin/java -Djava.net.preferIPv4Stack=true -DlibertySystems.configFile=Root.configFile.xml -jar Root.jar server Root.yml
