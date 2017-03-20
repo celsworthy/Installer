c9a8d7ac97ae9f09d41994c40c00f2a06a811689
Ian Hudson <ian@cel-uk.com>
2017-03-20 11:59:16 +0000

    Made getWifi script more robust

--- Root/getCurrentWifiState.ARM-32bit.sh
+++ Root/getCurrentWifiState.ARM-32bit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 poweredInput=$(sudo ifdown -n wlan0 2>&1 | sed 's/^[ \t]*//;s/[ \t]*$//' | awk '{print tolower($0)}' )
-associatedInput=$(iwconfig wlan0 | cut -d ' ' -f15 | sed 's/^[ \t]*//;s/[ \t]*$//' )
+associatedInput=$(iwconfig wlan0 2>/dev/null | cut -d ' ' -f15 | sed 's/^[ \t]*//;s/[ \t]*$//' )
 ssid=$(iwgetid wlan0 | cut -d : -f2 | sed 's/^[ \t]*//;s/[ \t]*$//')
 
 powered='false'
