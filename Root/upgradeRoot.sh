1fe74aad30a873633fb8891a70ba3f14f0a5027e
Tony Aldhous <tonyaldhous@cel-uk.com>
2018-10-09 12:33:06 +0100

    Improve layout of script.

--- Root/upgradeRoot.sh
+++ Root/upgradeRoot.sh
@@ -7,12 +7,12 @@
 #
 
 EXPAND_REQUIRED_FILE=/boot/expand_rfs
-ROOT_DIR="ARM-32bit"
+ROOT_ARM="ARM-32bit"
 ROOT_PARENT="/home/pi"
 ROOT_UPGRADE_FILE_BASE="/tmp/RootARM*"
 TMP_UPGRADE_DIR="/tmp/root-upgrade"
 
-ROOT_HOME=${ROOT_PARENT}/${ROOT_DIR}
+ROOT_HOME=${ROOT_PARENT}/${ROOT_ARM}
 
 ROOT_UPGRADE_SCRIPT=${ROOT_HOME}/Root/upgrade.sh
 
@@ -26,56 +26,56 @@ then
 	sudo reboot
 fi
 
-#Handle pre-run upgrade
+	#Handle pre-run upgrade
 for f in ${ROOT_UPGRADE_FILE_BASE}; do
-        if [ -e "$f" ]
-        then
-                upgradeFile=$(ls -t ${ROOT_UPGRADE_FILE_BASE} | head -1)
-                logger Found $upgradeFile - upgrading Robox Root
+	if [ -e "$f" ]
+	then
+		upgradeFile=$(ls -t ${ROOT_UPGRADE_FILE_BASE} | head -1)
+		logger Found $upgradeFile - upgrading Robox Root
 
-                # Unpack into temporary directory.
-                if [ -e $TMP_UPGRADE_DIR ]
-                then
-                        rm -rf  $TMP_UPGRADE_DIR
-                fi
-                unzip -o -d $TMP_UPGRADE_DIR $upgradeFile
+		# Unpack into temporary directory.
+		if [ -e $TMP_UPGRADE_DIR ]
+		then
+			rm -rf  $TMP_UPGRADE_DIR
+		fi
+		unzip -o -d $TMP_UPGRADE_DIR $upgradeFile
 
-                # Delete all downloaded zip files
-                rm -f ${ROOT_UPGRADE_FILE_BASE}
+		# Delete all downloaded zip files
+		rm -f ${ROOT_UPGRADE_FILE_BASE}
 
 		# Delete any old Root installation.
-                if [ -e ${ROOT_HOME}-1 ]
-                then
-                        rm -rf ${ROOT_HOME}-1
-                fi
+		if [ -e ${ROOT_HOME}-1 ]
+		then
+			rm -rf ${ROOT_HOME}-1
+		fi
 
 		# Move the current install out of the way.
-                mv $ROOT_HOME ${ROOT_HOME}-1
+		mv $ROOT_HOME ${ROOT_HOME}-1
 
 		# Move the new install from tmp to the correct place.
-                mv ${TMP_UPGRADE_DIR}/${ROOT_DIR} $ROOT_HOME
+		mv ${TMP_UPGRADE_DIR}/${ROOT_ARM} $ROOT_HOME
 
 		# Delete the tmp directory.
 		rm -rf  $TMP_UPGRADE_DIR
 
-                # The current directory has moved under our feet, so cd to the root directory.
+		# The current directory has moved under our feet, so cd to the root directory.
 		cd ${ROOT_HOME}/Root
 
-                # Run the upgrade script if one exists.
-                if [ -e ${ROOT_UPGRADE_SCRIPT} ]
-                then
-                        ${ROOT_UPGRADE_SCRIPT}
+		# Run the upgrade script if one exists.
+		if [ -e ${ROOT_UPGRADE_SCRIPT} ]
+		then
+			${ROOT_UPGRADE_SCRIPT}
 			# Delete the upgrade script so it is not run again.
-                        rm -f ${ROOT_UPGRADE_SCRIPT}
-                fi
+			rm -f ${ROOT_UPGRADE_SCRIPT}
+		fi
 
-                # Clear the Chromium cache
-                rm -rf ~/.cache/chromium/Default
+		# Clear the Chromium cache
+		rm -rf ~/.cache/chromium/Default
 
-                # Exit with status 1, causing a restart.
-                exit 1
-        fi
-        break
+		# Exit with status 1, causing a restart.
+		exit 1
+	fi
+	break
 done
 
 # Exit with status 0, to continue running as normal.
