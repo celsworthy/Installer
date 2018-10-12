1ffe5a9364921b2316976709165b098922527228
Tony Aldhous <tonyaldhous@cel-uk.com>
2018-10-12 13:45:43 +0100

    Provide offline installation of imagemagick and unclutter.

--- Root/unclutter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#export DISPLAY=:0.0
-#export XAUTHORITY=~/.Xauthority
-
-# Run unclutter to hide the cursor.
-export LD_LIBRARY_PATH=${ROOT_HOME}/animate/lib:${LD_LIBRARY_PATH}
-/home/pi/ARM-32bit/Root/x-apps/unclutter -idle 0.01 -root &
---
+++ Root/upgrade_data/offline/unclutter/install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo dpkg -i unclutter_8-20_armhf.deb
+sudo apt-get install -f
