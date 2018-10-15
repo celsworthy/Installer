52dcb3580dd469b4753e22ec4e089329855f4595
Tony Aldhous <tonyaldhous@cel-uk.com>
2018-10-15 16:25:26 +0100

    Wireless settings still not working - try restarting dhcp to force it to update. Remove "fixed" line from readme.

--- Root/setupWifi.ARM-32bit.sh
+++ Root/setupWifi.ARM-32bit.sh
@@ -17,19 +17,26 @@ else
 	then
 		sudo sed -i -e "/$startOfSection/,/$endOfSection/c\
 		${startOfSection}\n\
+	scan_ssid=1\n\
 	ssid=\"$ssid\"\n\
 	psk=\"$pw\"\n\
 ${endOfSection}" $fileToChange 
 	else
 		sudo sed -i "$ a ${startOfSection}\n\
+	scan_ssid=1\n\
 	ssid=\"$ssid\"\n\
 	psk=\"$pw\"\n\
 ${endOfSection}" $fileToChange
 	fi
 	sudo ip link set wlan0 down > /dev/null 2>&1
+	# Try to force wpa-supplicant to restart with the new configuration.
+	# At least one person on the interweb claimed this worked!
+	sudo systemctl daemon-reload
+	sudo systemctl restart dhcpcd
 	# Allow time for changes to come into effect.
 	sleep 1
+	# Bring up the interface.
 	sudo ip link set wlan0 up > /dev/null 2>&1
 	# Allow time for changes to come into effect.
-	sleep 6
+	sleep 10
 fi
