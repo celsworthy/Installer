d2ee5bebab81c942fde8d4a3a90b87da717a5c25
George Salter <george@cel-uk.com>
2019-01-18 11:01:11 +0000

    Add permissions for USB mounting files on upgrade

--- Root/createRootPackages.sh
+++ Root/createRootPackages.sh
@@ -77,6 +77,8 @@ doPackage()
 		find ${packagedir}/${applicationname} -regex ".*\.s[ho]" -exec chmod ug+x '{}' \;
 		# Set permissions of executables.		
 		find ${packagedir}/${applicationname} -regex ".*/bin/.*" -exec chmod ug+x '{}' \;
+		# Set permissions of usb mountung files.
+		find ${packagedir}/${applicationname} -regex "upgrade_data/usb_mount/.*" -exec chmod ug+x '{}' \;
 
         # Version number
         echo 'version = '${FINAL_BUILD_LABEL} > ${packagedir}/${applicationname}/application.properties
