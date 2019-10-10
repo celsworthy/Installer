a246da8b5b27a57eb4ee82b33a258192ddf39573
taldhous <tonyaldhous@cel-uk.com>
2019-10-10 16:00:41 +0100

    Calibrate touch screen according to type of Root installation.

--- GRoot/runGRoot.sh
+++ GRoot/runGRoot.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
+export ROBOX_PRO_FILE=/boot/RoboxPro
+
+if [ -e "$ROBOX_PRO_FILE" ]
+then
+	# This is a RoboxPro
+	X_MIN=3840
+	X_MAX=1600
+	Y_MIN=400
+	Y_MAX=6400
+
+else
+	# This is a Root/Mote.
+	X_MIN=300
+	X_MAX=2500
+	Y_MIN=4000
+	Y_MAX=-2300
+fi
+
 cd /home/pi/ARM-32bit/GRoot
-sudo /home/pi/ARM-32bit/Root/java/bin/java -Dmonocle.input.0/0/0/0.minX=3840 -Dmonocle.input.0/0/0/0.maxX=1600 -Dmonocle.input.0/0/0/0.minY=400 -Dmonocle.input.0/0/0/0.maxY=6400  -Dmonocle.input.0/0/0/0.flipXY=true -jar ./GRoot.jar -u 500 -s
+sudo /home/pi/ARM-32bit/Root/java/bin/java -Dmonocle.input.0/0/0/0.minX=$X_MIN -Dmonocle.input.0/0/0/0.maxX=$X_MAX -Dmonocle.input.0/0/0/0.minY=$Y_MIN -Dmonocle.input.0/0/0/0.maxY=$Y_MAX  -Dmonocle.input.0/0/0/0.flipXY=true -jar ./GRoot.jar -u 500 -s
