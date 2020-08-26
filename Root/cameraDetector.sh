53166dee13003cff5124a5074202bc136603b07d
taldhous <tonyaldhous@cel-uk.com>
2020-08-26 14:26:04 +0100

    Further developments for camera feature.

--- Root/cameraDetector.sh
+++ Root/cameraDetector.sh
@@ -9,6 +9,12 @@ do
 		echo NOT_CONNECTED
 		exit
 	fi
-	echo $device > /dev/stdout
-	echo " "
+	# Check that device has video capture capability, which is so when
+	# the least significant bit of the device capabilities is set.
+	devcaps=`v4l2-ctl --device=$device -D | grep "Device Caps" | cut -d 'x' -f2`
+	l=${devcaps: -1} # Get the last digit in the capabilites number - note space is required
+	if [[ "$((l%2))" = 0 ]]; then
+		echo $device > /dev/stdout
+		echo " "
+	fi
 done
