91af414a1f974af8ef7d3fcc781a36c59b9bc31a
taldhous <tonyaldhous@cel-uk.com>
2020-09-24 11:57:17 +0100

    Remove sensitive or unnecessary files ready for open source version.

--- makeAndSignWindowsDrivers.bat
+++
@@ -1,3 +0,0 @@
-cd windows_drivers
-inf2cat /v /driver:.\ /os:XP_X86,XP_X64,Vista_X86,Vista_X64,7_X86,7_X64,8_X86,8_X64,6_3_X86,6_3_X64
-signtool sign /v /d "Robox Drivers" /t http://timestamp.verisign.com/scripts/timestamp.dll roboxx86.cat roboxx64.cat
